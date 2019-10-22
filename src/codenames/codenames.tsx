1715e957de787168a0b6898e5ba7f6e5ae01266e
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2019-10-22 18:01:06 +0200

    Refactor whole component and code cleanup

--- src/codenames/codenames.tsx
+++ src/codenames/codenames.tsx
@@ -1,8 +1,5 @@
 import {
-  IWorkItemChangedArgs,
-  IWorkItemFieldChangedArgs,
   IWorkItemFormService,
-  IWorkItemLoadedArgs,
   WorkItemTrackingServiceIds
 } from "azure-devops-extension-api/WorkItemTracking";
 import * as SDK from "azure-devops-extension-sdk";
@@ -28,14 +25,17 @@ function getRandomOfArray(items:string[]) {
   return items[Math.floor(Math.random() * items.length)];
 }
 
-var output = console.log;
+var workItemFormService = SDK.getService<IWorkItemFormService>(
+  WorkItemTrackingServiceIds.WorkItemFormService
+);
 
 interface WorkItemFormGroupComponentState {
   eventContent: string;
-  id: string;
-  fields: string;
   nouns: string[][];
   adjectives_verbs: string[][];
+  fieldName: string;
+  info: string;
+  isDisabled: boolean;
 }
 
 class WorkItemFormGroupComponent extends React.Component<{},  WorkItemFormGroupComponentState> {
@@ -43,136 +43,76 @@ class WorkItemFormGroupComponent extends React.Component<{},  WorkItemFormGroupC
     super(props);
     this.state = {
       eventContent: "",
-      id: "",
-      fields: "",
       nouns: [],
-      adjectives_verbs: []
+      adjectives_verbs: [],
+      fieldName: "",
+      info: "",
+      isDisabled: true
     };
   }
 
   public componentDidMount() {
     SDK.init().then(() => {
-      this.registerEvents();
+      this.load();
     });
   }
-
+  
   public render(): JSX.Element {
     return (
-      <div>
-        <Button
-          className="sample-work-item-button"
-          text="Click me to change title!"
-          onClick={() => this.onClickSet()}
-        />
-        <Button
-          className="sample-work-item-button"
-          text="Click me to get id!"
-          onClick={() => this.onClickGet()}
-        />
-        <Button
-          className="sample-work-item-button"
-          text="Get fields"
-          onClick={() => this.onClickGetFields()}
-        />
-        <p>Hi there</p>
-        <p><div className="sample-work-item-events">{this.state.eventContent}</div></p>
-        <p><div className="sample-work-item-id">{this.state.id}</div></p>
-        <p><div className="sample-work-item-id">{this.state.fields}</div></p>
-      </div>
-    );
+    <div>
+      { this.state.info && <div><span>{this.state.info}</span></div> }
+      <Button
+        className="codenames-button"
+        text="🎲 Generate a codename"
+        onClick={() => this.onClickSet()}
+        disabled={this.state.isDisabled}
+      />
+    </div>
+    )
   }
 
-  private registerEvents() {
-    SDK.register(SDK.getContributionId(), () => {
-      return {
-        // Called when the active work item is modified
-        onFieldChanged: (args: IWorkItemFieldChangedArgs) => {
-          this.setState({
-            eventContent: `onFieldChanged - ${JSON.stringify(args)}`
-          });
-          output(JSON.stringify(this.state));
-        },
+  private load()
+  {
+    var getNouns = getFetch("./../nouns.json");
+    var getAdjectivesVerbs = getFetch("./../adjectives-verbs.json");
+    var fieldName = SDK.getConfiguration().witInputs["CodenamesFieldName"];
 
-        // Called when a new work item is being loaded in the UI
-        onLoaded: (args: IWorkItemLoadedArgs) => {
-          this.setState({
-            eventContent: `onLoaded - ${JSON.stringify(args)}`
-          });
-
-          getFetch("./../nouns.json").then(x => this.state.nouns.push(x));
-          getFetch("./../adjectives-verbs.json").then(x => this.state.adjectives_verbs.push(x));
-        },
-
-        // Called when the active work item is being unloaded in the UI
-        onUnloaded: (args: IWorkItemChangedArgs) => {
-          this.setState({
-            eventContent: `onUnloaded - ${JSON.stringify(args)}`
-          });
-          output(JSON.stringify(this.state));
-        },
-
-        // Called after the work item has been saved
-        onSaved: (args: IWorkItemChangedArgs) => {
-          this.setState({
-            eventContent: `onSaved - ${JSON.stringify(args)}`
-          });
-          output(JSON.stringify(this.state));
-        },
-
-        // Called when the work item is reset to its unmodified state (undo)
-        onReset: (args: IWorkItemChangedArgs) => {
-          this.setState({
-            eventContent: `onReset - ${JSON.stringify(args)}`
-          });
-          output(JSON.stringify(this.state));
-        },
-
-        // Called when the work item has been refreshed from the server
-        onRefreshed: (args: IWorkItemChangedArgs) => {
-          this.setState({
-            eventContent: `onRefreshed - ${JSON.stringify(args)}`
-          });
-          output(JSON.stringify(this.state));
+    Promise.all([getNouns, getAdjectivesVerbs]).then(x => {
+      if (x[0] && x[1])
+      {
+        this.state.nouns.push(x[0]);
+        this.state.adjectives_verbs.push(x[1]);
+        
+        if (fieldName)
+        {
+          this.setState({ fieldName: fieldName });
+          this.setState({ isDisabled: false });
         }
-      };
+        else
+        {
+          this.setState({ info:
+            "⚠️ It appears that Codenames doesn't know which field to update. " +
+            "Make sure that the field name was set in custom control configuration."
+          });
+        }
+      }
+      else
+      {
+        this.setState({ info:
+          "❌ Fatal error: There was a problem getting words definitions."
+        });
+      }
     });
   }
 
   private async onClickSet() {
-    const workItemFormService = await SDK.getService<IWorkItemFormService>(
-      WorkItemTrackingServiceIds.WorkItemFormService
-    );
+    if (this.state.fieldName)
+    {
+      var wordFirst = capitalizeFirstLetter(getRandomOfArray(this.state.adjectives_verbs[0]));
+      var wordSecond = capitalizeFirstLetter(getRandomOfArray(this.state.nouns[0]));
 
-    var wordFirst = capitalizeFirstLetter(getRandomOfArray(this.state.adjectives_verbs[0]));
-    var wordSecond = capitalizeFirstLetter(getRandomOfArray(this.state.nouns[0]));
-
-    workItemFormService.setFieldValue(
-      "Custom.Codenames", `${wordFirst} ${wordSecond}`
-    );
-  }
-
-
-
-  private async onClickGet() {
-    const workItemFormService = await SDK.getService<IWorkItemFormService>(
-      WorkItemTrackingServiceIds.WorkItemFormService
-    );
-    
-    //const workItemId = (await workItemFormService.getFieldValue('System.Id')) as string;
-    //this.setState({id: `id is ${workItemId}`})
-    //output(JSON.stringify(this.state));
-  }
-
-  private async onClickGetFields() {
-    const workItemFormService = await SDK.getService<IWorkItemFormService>(
-      WorkItemTrackingServiceIds.WorkItemFormService
-    );
-    const data = (await workItemFormService.getFields());
-    this.setState({fields: `Fields are ${JSON.stringify(data)}`})
-    output(JSON.stringify(this.state));
-
-    const witinputs = (await SDK.getConfiguration());
-    output("witinputs" + JSON.stringify(witinputs));
+      workItemFormService.then(x => x.setFieldValue(this.state.fieldName, `${wordFirst} ${wordSecond}`));
+    }
   }
 }
 
